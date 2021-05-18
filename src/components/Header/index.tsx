6e11443c2a750f22e811a189f617f0bc28791bab
Paulo Henrique <phvcandido@gmail.com>
2021-05-17 23:14:38 -0300

    Adicionando arquivos ide imagem
    Adicionando estilo ao header e nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
-
+import { ReactComponent as Dogs } from '../../assets/dogs.svg';
 
 function Header() {
   return (
     <div className={styles.Header}>
       <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar conta</Link>
+        <Link className="logo" to="/"><Dogs aria-label="Dogs - Home" /></Link>
+        <Link className={styles.login} to="/login">Login / Criar conta</Link>
       </nav>
     </div>
   )
 }
 
-export default Header
+export default Header;
