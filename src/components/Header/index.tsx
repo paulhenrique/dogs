bd337bd715e399c6aec94ab5de0a6fc139fe77c6
Paulo Henrique <phvcandido@gmail.com>
2021-06-03 19:00:22 -0300

    Atualizando Navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import styles from './styles.module.scss';
+// import styles from './styles.module.scss';
+import { Container, Button, Flex, Spacer, Box } from "@chakra-ui/react"
 import { ReactComponent as Dogs } from '../../assets/dogs.svg';
 
 function Header() {
   return (
-    <div className={styles.Header}>
+    <div>
       <nav className="container">
-        <Link className="logo" to="/"><Dogs aria-label="Dogs - Home" /></Link>
-        <Link className={styles.login} to="/login">Login / Criar conta</Link>
+        <Container>
+          <Box pt="5">
+            <Flex align="center">
+              <Box p="3"><Link className="logo" to="/"><Dogs aria-label="Dogs - Home" /></Link></Box>
+              <Spacer />
+              <Link to="/login">
+                <Button>Login/Criar Conta</Button>
+              </Link>
+            </Flex>
+          </Box>
+        </Container>
       </nav>
     </div>
   )
